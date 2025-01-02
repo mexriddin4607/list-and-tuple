27f827e66616f90b7d183d6ff401894c17c43764
mexriddin4607 <mexriddinavmedov63@gmail.com>
2025-01-02 21:52:04 +0500

    bajarildi

---
+++ Tuple_indexing/3-misol.py
@@ -0,0 +1,11 @@
+
+tuple = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+
+element_2 = tuple[1]  # 2-element (indeks 1)
+element_8 = tuple[7]  # 8-element (indeks 7)
+print("2-element:", element_2)
+print("8-element:", element_8)
+
+# Ularning yig'indisini hisoblash
+sum_elements = element_2 + element_8
+print("Yig'indisi:", sum_elements)
