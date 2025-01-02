19bc123763536938679fb75762df6ae764ff4ce3
mexriddin4607 <mexriddinavmedov63@gmail.com>
2025-01-02 22:23:56 +0500

    bajarildi

---
+++ Qo'shimcha_mashqlar/3-misol.py
@@ -0,0 +1,10 @@
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+new_list = list[::-3]
+
+my_tuple = tuple(new_list)
+
+element_index = my_tuple.index(7)
+
+print("Yangi tuple:", my_tuple)
+print("Element 7 indeksi:", element_index)
