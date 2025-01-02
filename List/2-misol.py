18301f6b0b1a9de6b3ecb3e2e326d5a95185b828
mexriddin4607 <mexriddinavmedov63@gmail.com>
2025-01-02 17:52:36 +0500

    bajarildi

---
+++ List/2-misol.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# append oxiriga yangi element qo'shadi
+my_list.append(11)
+
+# pop boshlang'ich elementni olib tashlaydi
+my_list.pop(0)
+
+print(my_list)
