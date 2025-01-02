694e1b219452feef7b25424c46c0c690380aee64
mexriddin4607 <mexriddinavmedov63@gmail.com>
2025-01-02 11:20:00 +0500

    bajarildi

---
+++ variableVAindentifier/3-misol.py
@@ -0,0 +1,8 @@
+2_age = 24
+#O‘zgaruvchi nomi raqam bilan boshlanmaydi.
+
+my name = mexriddin
+#Identifier nomida bo‘shliq bo‘lishi mumkin emas.
+
+my-surnema = axmedov
+#Identifier nomida maxsus belgi - ishlatilmaydi.
